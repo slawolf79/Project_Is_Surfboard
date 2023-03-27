ffa040b33e0612413c5feca9892ffe26cec692a5
slawolf79 <122656917+slawolf79@users.noreply.github.com>
2023-03-27 20:17:22 +0300

    Update hamburger-menu.js

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -4,7 +4,8 @@ const fullMenu = document.querySelector('#hamburgerMenu');
 // при клике на список меню
 hamMenu.addEventListener("click", (e) => {
     e.preventDefault();
-    hamMenu.classList.toggle('active');
+    
+    fullMenu.classList.toggle('active');
     
 
 
